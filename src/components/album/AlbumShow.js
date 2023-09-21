aad75c0406cec23283b08ba53a75eb4316916248
rstruthers <rachel.struthers@sap.com>
2023-09-21 17:00:08 -0500

    Minor changes. Added insomnia file

--- src/components/album/AlbumShow.js
+++ src/components/album/AlbumShow.js
@@ -1,7 +1,13 @@
+import {Card} from "react-bootstrap";
+import albumImage from "../../images/book.png";
+
 const AlbumShow = ({album}) => {
-    return <div>
-        {album.name}
-    </div>
+    return <Card style={{ width: '16rem'}} className="book-show" >
+        <Card.Img src={albumImage}
+        />
+        <Card.Title>{album.name}</Card.Title>
+    
+    </Card>
 }
 
 export default AlbumShow;
--- src/services/jsonServerApi.js
+++ src/services/jsonServerApi.js
@@ -1,11 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+
 export const jsonServerApi = createApi({
     reducerPath: 'jsonServerApi',
     baseQuery: fetchBaseQuery({ baseUrl: `${process.env.REACT_APP_BASE_API_URL}`} ),
     endpoints: (builder) => ({
         getAlbums: builder.query({
-            query: () => `albums`,
+            query: () => `albums`
         }),
     }),
 });
